74ab26c1bd4b94bf1f19189e70b308dedee7d43b
zorkadorka <zora.moravcikova@gmail.com>
2015-02-17 21:33:57 +0100

    fotos

---
+++ wp-content/themes/technik/page-onas.php
@@ -0,0 +1,31 @@
+<?php
+/*
+Template Name: O_nas
+*/
+
+get_header(); ?>
+
+<section class="Onas">
+<?php 
+if ( have_posts() ) {
+	while ( have_posts() ) {
+		the_post(); 
+?>
+	<article>
+		<?php the_content(); ?>
+	</article>
+<?php
+	} // end while
+} // end if
+?>
+</section>
+
+</section> <!-- .main-content -->
+
+<aside class="content aside-content">
+	<?php get_sidebar(); ?>
+</aside>
+
+</div> <!-- .wrap -->
+
+<?php get_footer(); ?>

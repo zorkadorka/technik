6d76d6ec6003cb1610e7c0127f1e0502c4977930
Jarino <xloebl@stuba.sk>
2015-03-16 13:45:17 +0100

    fixed header and footer images

--- wp-content/themes/technik/footer.php
+++ wp-content/themes/technik/footer.php
@@ -2,10 +2,12 @@
 
 <footer>
 	<div class="separator"></div>
-	
-	<p>FS Technik - ANNO 1953 - Powered By Wordpress</p>
-
-
+	<div class="footer-image">
+		<img src="<?= get_bloginfo('template_url') ?>/images/mosaic.jpg">
+	</div>
+	<div class="footer-content">
+		<p>FS Technik - ANNO 1953 - Powered By Wordpress</p>
+	</div>
 </footer>
 
 

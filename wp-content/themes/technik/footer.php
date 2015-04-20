96840674c1f30189a6b05ae2a2ec631be9c3fb84
martinpreky <martin.prekala@gmail.com>
2015-04-20 10:44:45 +0200

    header a footer zmena fotiek

--- wp-content/themes/technik/footer.php
+++ wp-content/themes/technik/footer.php
@@ -3,7 +3,7 @@
 <footer>
 	<div class="separator"></div>
 	<div class="footer-image">
-		<img src="<?= get_bloginfo('template_url') ?>/images/mosaic.jpg">
+		<img src="<?= get_bloginfo('template_url') ?>/images/mozaika.jpg">
 	</div>
 	<div class="footer-content">
 		<p>FS Technik STU - ANNO 1953 - Powered By Wordpress</p>
